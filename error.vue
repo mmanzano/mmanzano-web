ca53540e9c758410529a767e5b64a534431620bd
Miguel Manzano García <mmanzano@gmail.com>
2022-12-29 20:43:04 +0100

    Better error handling

--- error.vue
+++ error.vue
@@ -1,20 +1,32 @@
 <script setup lang="ts">
-defineProps({
-  error: Object
+const props = defineProps({
+  error: Object,
 })
 
 useHead({
   title: 'Miguel Manzano - Página no encontrada',
   meta: [{ name: 'og:title', content: 'Miguel Manzano - Página no encontrada' }]
 })
+const { lang } = useSupportedNavigatorLanguage()
+let errorMessage = props.error?.message;
+if (props.error?.statusCode === '404') {
+  errorMessage = lang === 'es' ? 'Página no encontrada' : 'Page not found'
+}
+
+const goHome = lang === 'es' ? 'Volver a Casa' : 'Go Home'
 </script>
 
 <template>
-  <div class="container mx-auto p-10">
-    <MainHeader />
-    <h1>{{ error.message }}</h1>
-    <NuxtLink to="/">
-      Volver a casa
-    </NuxtLink>
-  </div>
+  <ClientOnly>
+    <div class="w-full">
+      <MainHeader />
+      <div class="min-h-screen bg-yellow-100 px-10 py-2 text-xl">
+        <h1 class="px-10 mt-10 mb-10"><strong>{{ errorMessage }}</strong></h1>
+
+        <NuxtLink to="/" class="px-10 text-underline">
+          {{ goHome }}
+        </NuxtLink>
+      </div>
+    </div>
+  </ClientOnly>
 </template>
--- pages/[lang]/blog/[slug].vue
+++ pages/[lang]/blog/[slug].vue
@@ -5,8 +5,15 @@ const ArticleStore = useArticleStore()
 
 const route: RouteLocationNormalizedLoaded = useRoute()
 const { lang } = useSupportedNavigatorLanguage()
-const slug: string = usePropertyFromRoute(route, 'slug') as string
+const slug: string | null = usePropertyFromRoute(route, 'slug')
+if (slug === null) {
+  throw Error('Something is not working properly. We are not getting the slug. Sorry, can you try again.')
+}
 await ArticleStore.getArticle(lang, slug)
+
+if (!ArticleStore.article) {
+  throw createError({ statusCode: 404 })
+}
 useHead({
   title: ArticleStore.article?.title,
   meta: [
