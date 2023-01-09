6d62862ac8f05171490d956c1f54d6025bfdc546
Miguel Manzano García <mmanzano@gmail.com>
2023-01-09 18:51:21 +0100

    Move content logic to composable

--- pages/bitacora/index.vue
+++ pages/bitacora/index.vue
@@ -1,26 +1,21 @@
 <script setup lang="ts">
-import { useArticleStore } from "@/stores/ArticleStore"
-const ArticleStore = useArticleStore()
-
 definePageMeta({
   middleware: [
     'blog-is-visible',
   ],
 })
-
-useHead({
-  title: 'Blog',
-})
-
-const route = useRoute()
-await ArticleStore.getArticleList(route.fullPath.split('/')[1])
 defineI18nRoute({
   locales: ['es']
 })
+useHead({
+  title: 'Bitácora',
+})
+
+const articleList = await useBlogIndex()
 </script>
 
 <template>
   <div>
-    <ArticleList :articles="ArticleStore.articleList" />
+    <ArticleList :articles="articleList" />
   </div>
 </template>
