636168f6ecf8e6abefb01e466ae90796ebdec970
Miguel Manzano García <mmanzano@gmail.com>
2023-06-12 15:12:41 +0200

    Hide Lab Section

--- components/MainHeaderNavigation.vue
+++ components/MainHeaderNavigation.vue
@@ -11,9 +11,9 @@ const language = route.fullPath.split('/')[1]
       <li class="mr-2">
         <NuxtLink :to="language === 'en' ? '/en' : '/es'">{{ t('home', language) }}</NuxtLink>
       </li>
-      <li class="mr-2">
+      <!-- <li class="mr-2">
         <NuxtLink :to="language === 'en' ? '/en/lab' : '/es/lab'">Lab</NuxtLink>
-      </li>
+      </li> -->
       <li>
         <NuxtLink :to="language === 'en' ? '/en/blog' : '/es/bitacora'" v-if="config.public.blogIsVisible">{{
           t('blog')
