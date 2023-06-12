02e010cd504f4bc5686d6a59ea47bfd473035f33
Miguel Manzano García <mmanzano@gmail.com>
2023-06-12 16:17:02 +0200

    Remove tailwind used directly in class from vue files except from ArticleItem

--- components/content/ServiceListItem.vue
+++ components/content/ServiceListItem.vue
@@ -1,11 +1,32 @@
 <template>
-    <div class="bg-yellow-400 p-4 rounded-lg">
-        <div class="text-bold text-2xl pb-4 border-b-2 border-white">
+    <div class="service-container">
+        <div class="service-container__title">
             <slot />
         </div>
 
-        <div class="mt-2 pt-4">
+        <div class="service-container__body">
             <slot name="body"></slot>
         </div>
     </div>
 </template>
+
+<style scoped>
+.service-container {
+    padding: 1rem;
+    background-color: rgb(250 204 21 / 1);
+    border-radius: 0.5rem;
+}
+
+.service-container__title {
+    font-size: 1.5rem;
+    line-height: 2rem;
+    padding-bottom: 1rem;
+    border-bottom-width: 2px;
+    border-color: rgb(255 255 255 / 1);
+}
+
+.service-container__body {
+    margin-top: 0.5rem;
+    padding-top: 1rem;
+}
+</style>
