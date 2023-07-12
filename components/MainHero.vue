485ee7df25700a4f2b1d9fab754750eb23f38928
Miguel Manzano García <mmanzano@gmail.com>
2023-07-13 00:09:32 +0200

    configure fonts + think about hero

--- components/MainHero.vue
+++ components/MainHero.vue
@@ -5,9 +5,13 @@ const { t } = useI18n();
 <template>
   <div>
     <div class="hero">
-      <h1 class="moto">
-        {{ t('moto') }}
-      </h1>
+      <div class="moto-content">
+        <h1 class="moto">
+            {{ t('moto') }}
+        </h1>
+        <div class="semicolon">;</div>
+      </div>
+
       <h2 class="creator">
         Miguel Manzano García
       </h2>
@@ -20,29 +24,73 @@ const { t } = useI18n();
 
 <style scoped>
 .hero {
-  background-color: rgb(254 240 138 / 1);
-  padding: 2.5rem;
+  padding: 0.1rem 2.5rem;
+}
+
+.moto-content {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
 }
 
 .moto {
-  font-size: var(--font-size-5);
-  line-height: 2.5rem;
+  font-family: 'RhodiumLibre', 'Roboto', Arial, Helvetica, sans-serif;
+  padding: 0rem 0.5rem 0.5rem;
+  text-align: center;
+  font-size: var(--font-size-3);
+}
+
+.semicolon {
+  display: none;
 }
 
 .creator {
-  font-size: var(--font-size-2);
-  line-height: 1.75rem;
+  font-family: 'RhodiumLibre', 'Roboto', Arial, Helvetica, sans-serif;
+  padding: 0.2rem;
+  margin-bottom: 0.5em;
+  text-align: center;
+  font-size: var(--font-size-1);
 }
 
 .headline {
-  background-color: rgb(253 224 71 /1);
-  padding-left: 1.25rem;
-  padding-right: 1.25rem;
-  padding-top: 0.75rem;
-  padding-bottom: 0.75rem;
+  font-family: 'RhodiumLibre', 'Roboto', Arial, Helvetica, sans-serif;
+  margin: 0 auto;
+  padding: 0.5rem 0.3em;
+  max-width: 35rem;
   text-align: center;
-  font-size: var(--font-size-3);
-  line-height: 2rem;
-  color: rgb(55 65 81 / 1);
+  font-size: var(--font-size-2);
+  border-top: 2px solid var(--headline-bg-color);
+  border-bottom: 2px solid var(--headline-bg-color);
+}
+
+@media(min-width: 750px) {
+  .moto {
+    padding: 0rem 0.5rem 0.5rem;
+    font-size: var(--font-size-5);
+    text-align: left;
+  }
+
+  .creator {
+    padding: 0rem 0.5rem 2rem;
+    margin-bottom: 0.5em;
+    font-size: var(--font-size-3);
+    text-align: left;
+  }
+
+  .headline {
+    font-size: var(--font-size-4);
+  }
+
+  .semicolon {
+    display: inline-block;
+    width: fit-content;
+    height: fit-content;
+    font-family: 'Kablammo', 'Roboto', Arial, Helvetica, sans-serif;
+    transform: rotate(-30deg);
+    margin-top: -35px;
+    font-size: 70px;
+    font-weight: normal;
+    color: black;
+  }
 }
 </style>
